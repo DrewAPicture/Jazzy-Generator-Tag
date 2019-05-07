6bca1feb7094ec86558335a8982ac810889f68bc
Drew Jaynes <info@drewapicture.com>
2019-05-07 00:37:51 -0600

    Bump the version to 5.1.0.

--- jazzy-generator-tag.php
+++ jazzy-generator-tag.php
@@ -6,7 +6,7 @@
  * Author: Drew Jaynes
  * Author URI: http://werdswords.com
  * License: GPLv2
- * Version: 4.8.0
+ * Version: 5.1.0
  * Text Domain: jazzy-generator-tag
  * Domain Path: /languages/
  */
